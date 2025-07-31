95b98d25c06186b3b2eefccd1b7324cf50089ca2
Lucas <lucas.m.ferreira16@aluno.senai.br>
2025-07-31 13:30:05 -0300

    Subindo a Api Das reservas

---
+++ Reservas.Teste/MockData/Reservas-MockData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Reservas.Api.Models;
+
+namespace Reservas.Teste.MockData
+{
+    public class ReservasMockData
+    {
+        public static List<Reserva> GetReservas()
+        {
+
+            return new List<Reserva>()
+            {
+                new Reserva { ReservaId = 1, Nome = "Marcos", InicioLocacao = "São Paulo", FimLocacao = "Campinas" },
+
+                new Reserva { ReservaId = 1, Nome = "Willian Bonner", InicioLocacao = "Campinas", FimLocacao = "Minas Gerais" },
+
+                new Reserva { ReservaId = 1, Nome = "Luan Santana", InicioLocacao = "Chile", FimLocacao = "Paraguai" },
+            };
+        }
+    }
+}
